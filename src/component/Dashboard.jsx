cf30f3fa924ad9f6f0c8e053f739bf09206f4000
sanket <sanketmosamkar573@gmail.com>
2024-08-23 22:45:54 +0530

    Completed with Add Course and Add instance

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -6,13 +6,38 @@ const Dashboard = () => {
             <div className="infoContainer">
                 <aside className='leftAside'>
                     <div className='course_info'>
-                        <input type="text" name="courseTitle" className="infoFields" placeholder='Course title'/>
+                        <input type="text" name="courseTitle" className="infoFields" placeholder='Course title' />
                         <input type="text" name="courseCode" className="infoFields" placeholder='Course code' />
-                        <input type="text" name="courseDesc" className="infoFields" placeholder='Course description'/>
+                        <input type="text" name="courseDesc" className="infoFields" placeholder='Course description' />
                         <div className='course_btn'><button className='addCourseBtn'>Add course</button></div>
                     </div>
                 </aside>
-                <aside className='rightAside'></aside>
+                <aside className='rightAside'>
+                    <div className="courseInfoContainer">
+                        <div className="courseYearDropdown">
+                            <p>Select Course Name:</p>
+                            <select name="course" id="course">
+                                {
+                                    ["Select a course", "Java", "Python", "C#", "React Js"].map((value, i) => {
+                                        return (
+                                            <option disabled={i === 0} selected={i === 0} value={value} key={i}>{value}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
+                        <div className='courseYear'>
+                            {/* <div> */}
+                                <input type="number" className='yearNSem' name="year" placeholder='Year' />
+                                <input type="number" className='yearNSem' name="semester" placeholder='Semester' />
+                            {/* </div> */}
+                        </div>
+                        <div className='course_btn'><button className='addCourseBtn'>Add instance</button></div>
+
+
+
+                    </div>
+                </aside>
 
             </div>
             <div></div>
